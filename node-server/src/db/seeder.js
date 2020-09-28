9546168ac7883364f26db1c824dd092cac8ab495
Mustafa Mahmutovic <mustafa.mahmutovic93@gmail.com>
2020-09-28 15:16:12 +0200

    Added seeder with encrypted data

--- node-server/src/db/seeder.js
+++ node-server/src/db/seeder.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
 const db = require('./db');
 const users = require('./data.json');
 
 const { User } = db;
 
 User.insertMany(users);
+// eslint-disable-next-line no-console
+console.log('Database seeded! Press CTRL+X to exit the process :)');
